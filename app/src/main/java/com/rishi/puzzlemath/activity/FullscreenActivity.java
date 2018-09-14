7b74c12715ae17e5420602da7b2b4155ab0f286c
Rishikesh Pathak <rishikesh.p@sonimtech.com>
2018-09-14 19:31:22 +0530

    Created Custom Views

--- app/src/main/java/com/rishi/puzzlemath/activity/FullscreenActivity.java
+++ app/src/main/java/com/rishi/puzzlemath/activity/FullscreenActivity.java
@@ -3,6 +3,7 @@ package com.rishi.puzzlemath.activity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.rishi.puzzlemath.R;
@@ -11,6 +12,7 @@ import com.rishi.puzzlemath.util.Util;
 
 public class FullscreenActivity extends AppCompatActivity {
     private TextView mNumberTextview;
+    private LinearLayout mEmptyTvsLayout;
     private Util mUtil;
 
 
@@ -26,6 +28,7 @@ public class FullscreenActivity extends AppCompatActivity {
         mUtil = new Util();
         initViews();
         setNumberValue();
+        createDynamicAddViews();
     }
 
     private void setNumberValue() {
@@ -52,6 +55,31 @@ public class FullscreenActivity extends AppCompatActivity {
 
     private void initViews() {
         mNumberTextview = findViewById(R.id.tv_value);
+        mEmptyTvsLayout = findViewById(R.id.emptyTvsLayout);
+    }
+    private void createDynamicAddViews()
+    {
+
+        LinearLayout.LayoutParams lparams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        LinearLayout linearLayout = new LinearLayout(this);
+        linearLayout.setOrientation(LinearLayout.HORIZONTAL);
+        linearLayout.setLayoutParams(lparams);
+
+        TextView tv=new TextView(this);
+        tv.setText("test");
+
+        TextView tv2=new TextView(this);
+        tv2.setText("test2");
+
+        TextView tv3=new TextView(this);
+        tv3.setText("test3");
+
+        linearLayout.addView(tv);
+        linearLayout.addView(tv2);
+        linearLayout.addView(tv3);
+        mEmptyTvsLayout.addView(linearLayout);
     }
 
 }
--- app/src/main/java/com/rishi/puzzlemath/util/Util.java
+++ app/src/main/java/com/rishi/puzzlemath/util/Util.java
@@ -9,6 +9,10 @@ public class Util {
     private String TAG = Util.class.getSimpleName();
     private int min;
     private int max;
+    private int level;
+    private ArrayList<String> mMultipleList;
+    private ArrayList<String> mAdditionList;
+    private ArrayList<String> mDevisionList;
 
     public int getLevel() {
         return level;
@@ -17,8 +21,6 @@ public class Util {
     public void setLevel(int level) {
         this.level = level;
     }
-
-    private int level;
 
     public int getMin() {
         return min;
@@ -39,12 +41,39 @@ public class Util {
     public int getRandomNumber(int min, int max){
         return new Random().nextInt((max - min) + 1) + min;
     }
+    public String getAdditonProb()
+    {
+        String mAdditionProb = "";
+        if (mAdditionList == null || mAdditionList.size() == 0)
+            return mAdditionProb;
+        mAdditionProb = mAdditionList.get(getRandomValueForList(mAdditionList.size()-1));
+        return mAdditionProb;
+    }
+    public String getMulProb()
+    {
+        String mMultiplicationProb = "";
+        if (mMultipleList == null || mMultipleList.size() == 0)
+            return mMultiplicationProb;
+        mMultiplicationProb = mMultipleList.get(getRandomValueForList(mMultipleList.size()-1));
+        return mMultiplicationProb;
+    }
+    public String getDevProb()
+    {
+        String mDevisionProb = "";
+        if (mDevisionList == null || mDevisionList.size() == 0)
+            return mDevisionProb;
+        mDevisionProb = mDevisionList.get(getRandomValueForList(mDevisionList.size()-1));
+        return mDevisionProb;
+    }
+    private int getRandomValueForList(int listSize){
+        return new Random().nextInt((listSize - 1) + 1) + 1;
+    }
     public void getPossibleList(int number)
     {
         int mNumber = number;
-        ArrayList<String> mMultipleList = new ArrayList<>();
-        ArrayList<String> mAdditionList = new ArrayList<>();
-        ArrayList<String> mDevisionList = new ArrayList<>();
+        mMultipleList = new ArrayList<>();
+        mAdditionList = new ArrayList<>();
+        mDevisionList = new ArrayList<>();
 
        for (int i = 1; i <= mNumber ;i++)
         {
@@ -59,7 +88,7 @@ public class Util {
                    Log.d(TAG,"possible lists add are :"+i+"+"+j);
                }
            }
-            int mul = 0;
+           int mul = 0;
            if (i > 1) {
                mul = i * mNumber;
                mDevisionList.add(mul+"/"+i);
@@ -71,5 +100,10 @@ public class Util {
         Log.d(TAG,"mAdditionList IS :"+mAdditionList.toString());
         Log.d(TAG,"mDevisionList IS :"+mDevisionList.toString());
 
+    }
+    public void clearAllData(){
+        mMultipleList = null;
+        mAdditionList = null;
+        mDevisionList = null;
     }
 }
